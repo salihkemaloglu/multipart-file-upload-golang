739c726ba13d2df721a4b2adf7a3328c1b8d1826
salihkemaloglu <logiwa@DESKTOP-IQH06CH>
2019-10-25 15:54:24 +0300

    create bucket

--- main.go
+++ main.go
@@ -8,84 +8,58 @@ import (
 	"io"
 	"net/http"
 
+	"cloud.google.com/go/storage"
 	"github.com/rs/cors"
 	"github.com/spf13/pflag"
 	"goji.io"
 	"goji.io/pat"
-
-	"cloud.google.com/go/storage"
-	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
+
+var projectID = "gifted-outrider-244523"
+var client *storage.Client
 
 //SayHello ...
 func SayHello(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Service is working for SayHello...Received rpc from client \n")
-	explicit()
+	defer recoverPanic()
+	// name := fmt.Sprintf("golang-example-buckets-%d", time.Now().Unix())
+	if err := deleteBucketGCS("golang-example-buckets-1571989011"); err != nil {
+		panic(err)
+	}
 	respondWithJSON(w, http.StatusOK, map[string]string{"result": "Hello  service is working..."})
 }
 
-type File struct {
-	Name               string `json:"Name"`
-	ContentType        string `json:"ContentType"`
-	ContentLanguage    string `json:"ContentLanguage"`
-	CacheControl       string `json:"CacheControl"`
-	EventBasedHold     bool   `json:"EventBasedHold"`
-	TemporaryHold      bool   `json:"TemporaryHold"`
-	PredefinedACL      string `json:"PredefinedACL"`
-	Owner              string `json:"Owner"`
-	Size               int32  `json:"Size"`
-	ContentEncoding    string `json:"ContentEncoding"`
-	ContentDisposition string `json:"ContentDisposition"`
-	Generation         int16  `json:"Generation"`
-	Metageneration     int16  `json:"Metageneration"`
-	StorageClass       string `json:"StorageClass"`
-	CustomerKeySHA256  string `json:"CustomerKeySHA256"`
-	KMSKeyName         string `json:"KMSKeyName"`
-	Prefix             string `json:"Prefix"`
-	Etag               string `json:"Etag"`
-}
-
-// Authenticate to Google Cloud Storage and return handler
-func explicit() []File {
+func createBucketGCS(bucketName string) error {
 	ctx := context.Background()
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile("My First Project-cd720c50273d.json"))
-	defer recoverPanic()
-	if err != nil {
+	// [START create_bucket]
+	if err := client.Bucket(bucketName).Create(ctx, projectID, nil); err != nil {
 		panic(err)
 	}
-	it := client.Bucket("gignox_bucker-001").Objects(ctx, nil)
-	// var files []File
-	for {
-		attrs, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		defer recoverPanic()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(attrs)
-		fmt.Println(attrs.Name)
-	}
+	// [END create_bucket]
 	return nil
 }
+func deleteBucketGCS(bucketName string) error {
+	ctx := context.Background()
+	// [START delete_bucket]
+	if err := client.Bucket(bucketName).Delete(ctx); err != nil {
+		return err
+	}
+	// [END delete_bucket]
+	return nil
+}
+
 func recoverPanic() {
 	if err := recover(); err != nil {
 		fmt.Println("There's something wrong:", err)
 	}
 }
-func writeToCloudStorage(file []byte, fileName string) error {
+
+func writeToBucketGCS(file []byte, fileName string) error {
+
 	ctx := context.Background()
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile("My First Project-cd720c50273d.json"))
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-
-	bucket := client.Bucket("gignox_bucker-001")
-
+	bucket := client.Bucket("golang-example-buckets-1571989011")
 	wc := bucket.Object(fileName).NewWriter(ctx)
 	wc.ContentType = "text/plain"
 
@@ -106,7 +80,7 @@ func writeToCloudStorage(file []byte, fileName string) error {
 func UploadFile(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Service is working for UploadFile...Received rpc from client \n")
-
+	defer recoverPanic()
 	file, handler, err := r.FormFile("file")
 	if err != nil {
 		respondWithError(w, http.StatusUnsupportedMediaType, err.Error())
@@ -122,15 +96,16 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer file.Close()
-	// copy example
 
 	buf := bytes.NewBuffer(nil)
 	if _, err := io.Copy(buf, file); err != nil {
 		respondWithError(w, http.StatusUnsupportedMediaType, err.Error())
 		return
 	}
-	err1 := writeToCloudStorage(buf.Bytes(), handler.Filename)
-	_ = err1
+	defer recoverPanic()
+	if err := writeToBucketGCS(buf.Bytes(), handler.Filename); err != nil {
+		panic(err)
+	}
 	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
 func main() {
@@ -143,7 +118,13 @@ func main() {
 		Addr:    fmt.Sprintf(":%v", 8900),
 		Handler: cors.AllowAll().Handler(mux),
 	}
-
+	fmt.Println("GCS Service Started")
+	ctx := context.Background()
+	var err error
+	client, err = storage.NewClient(ctx, option.WithCredentialsFile("My First Project-cd720c50273d.json"))
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("server started as http and listen to port: %v \n", 8900)
 	if err := httpServer.ListenAndServe(); err != nil {
 		fmt.Printf("failed starting http server: %v", err.Error())
